6372a88053d474eaff6f40c33ce50bf243243553
jamesafross <jamesafross@hotmail.co.uk>
2020-10-17 13:00:01 +0100

    .

--- PDE/1_Pop_2D/Run.jl
+++ PDE/1_Pop_2D/Run.jl
@@ -3,31 +3,37 @@ using SparseArrays
 using ProgressMeter
 using DifferentialEquations
 using NPZ
+#Include Files
 include("banded_matrices.jl")
 include("RHS_funcs_wizardHat.jl")
 include("functions.jl")
 include("find_SS.jl")
-
-X_max = 2pi;
-dx = pi/(2^8)
-T_max = 1000
+#spatial parameters
+X_max = 8pi;
+dx = pi/(2^6)
+T_max = 600
 dxdx = dx * dx;
 X = Int(2*X_max / dx);
-#params
-v = 2
-eta_0 =-0.1
-Delta = 0.5
-alfa = 3
-kappaV =1
-kappaS = -15
-tau = 1
-k_c = 1
 Dxx = (1 / dxdx) * D2x(X)
 X_space = LinRange(-X_max,X_max,X)
+#model parameters
+v = 3    # axonal velocity
+eta_0 =0.3 # mean drive
+Delta = 0.5 # coherence
+alfa = 3 # synaptic time constant
+kappaV =1.2 # gap junction strength
+kappaS = 5 #synaptic coupling strength
+tau = 1 # membrane time constant
+
+
+k_c = 1 #wave length to excite (initial conditions)
+
 print(X)
+#steady state
 zeroRV = SteadyState()
 print("\n Fixed points at: \n R = ", zeroRV[1], " \n V = ", zeroRV[2],"\n")
 
+#initialise initial conditions
 R0 = zeros(X)
 V0=zeros(X)
 A10=zeros(X)
@@ -37,7 +43,9 @@ A40=zeros(X)
 psi0 = zeros(X)
 g0 = zeros(X)
 p0=zeros(X)
-perturb =  .01*cos.(k_c * X_space)
+#perturbation to the steady state
+perturb =  0.0001*cos.(X_space)
+#initial conditions
 R0 .= zeroRV[1] .+ 1*perturb
 V0 .= zeroRV[2] .+ 1*perturb
 psi0 .= 0 .+ 1*perturb
@@ -45,6 +53,8 @@ A10 .= 0 .+ 1*perturb
 A20 .= 0 .+ 1*perturb
 p0 .= 0 .+ 1*perturb
 g0 .= 0 .+ 1*perturb
+
+#initial conditions of the solver
 u0 = zeros(8 * X)
 u0[1:X] .= R0
 u0[X + 1:2 * X] .= V0
@@ -67,22 +77,25 @@ R = sol[1:X,:]
 V = sol[X + 1:2*X,:]
 g = sol[7*X+1:8*X,:]
 
+#conversion to kuromoto frame
 W = pi*R + im*V
 Z = (1 .-conj.(W)) ./(1 .+conj.(W))
 sync = abs.(Z)
 syncSave = sync[:,end-200:end]
 RSave = R[:,end-200:end]
 
+
+#save file, plotting, etc
 #@gif for i = 1:length(syncSave[1,:])
 #  plot(syncSave[:,i],ylims=[minimum(syncSave),maximum(syncSave)])
 #end
 print(size(syncSave))
 
-print("save? (Y/N) ")
-ans = readline()
-if ans == "Y"
-npzwrite("/home/james/PhD_Work/Python_Code/Brain_Top_Paper/Review_Paper/data/1D_sync_Turing.npy", syncSave)
-end
+#print("save? (Y/N) ")
+#ans = readline()
+#if ans == "Y"
+#npzwrite("/home/james/PhD_Work/Python_Code/Brain_Top_Paper/Review_Paper/data/1D_sync_Turing.npy", syncSave)
+#end
 
-heatmap(syncSave)
-@gif for i = 1:2000; plot(sync[:,i],ylims=[0,1]);end
+heatmap(syncSave[:,1:end])
+#@gif for i = 1:2000; plot(sync[:,i],ylims=[0,1]);end
--- PDE/1_Pop_3D/Run.jl
+++ PDE/1_Pop_3D/Run.jl
@@ -12,20 +12,21 @@ include("find_SS.jl")
 
 #Editable parameters
 #Spatial discretisation
-X_max = 4pi #m
-X_min = -4pi
-N =  2^7
-T_max = 50
+X_max = pi #m
+X_min = -pi
+N = 200
+T_max =100
 #params
 v = 2
-eta_0=6
+eta_0=5
 Delta = 0.5
 alfa = 5.0
-kappaV = 0.3
+kappaV = 0.5
 kappaS = 12.0
 tau = 1
 tau_a = 0.5
 delta = 0.0
+saveat = .1
 
 
 #Spatially discretisation
@@ -52,11 +53,14 @@ p0=zeros(XX)
 a0 = zeros(XX)
 R0 = reshape(R0,X,X)
 
+
 perturb = zeros(X,X)
-perturb =  real(0.01*init_conds(1,3,[0,2pi/3,4pi/3],[1,1,1],Xspace,Xspace'))
-sig = 2
-guass_init = exp.(-(Xspace.^2)/sig .-((Xspace').^2)/sig)
-perturb = reshape(guass_init,XX)
+gauss_init = exp.(-Xspace.^2 .- (Xspace').^2)
+perturb =  real(0.01*init_conds(3,3,[pi/2,2pi/3,4pi/3],[1,3,3],Xspace,Xspace'))
+#perturb = gauss_init
+#perturb = 0.01*cos.(3*Xspace)*cos.(3*Xspace')
+
+perturb = reshape(perturb,XX)
 
 R0 = zeroRV[1] .+ perturb
 V0 = zeroRV[2] .+ perturb
@@ -72,7 +76,7 @@ a0 = perturb
 #R0=5*rand(XX)
 print("\n Use end of last simulation as initial conditions? (y/N)")
 ans1 = readline(stdin)
-if ans1 == "Y"
+if ans1 == "Y" || ans1 == "y"
     #u0 = sol[:,end]
     u0 = zeros(9 * XX)
     #u0 = load("endsim.jld","arr")
@@ -81,8 +85,6 @@ if ans1 == "Y"
     Dyy = (1/dxdx)*D2r2(X, XX)
     Dxxyy = Dxx+Dyy
     #print(size(u0))
-elseif ans1 == "y"
-    u0 = sol[:,end]
 else
 print("\n Making sparse operator matrices ...... \n")
 Dxx = (1/dxdx)*D2r1(X, XX)
@@ -113,7 +115,7 @@ tspan = (0.0, T_max)
 params = [tau,tau_a,kappaV,kappaS, eta_0, alfa, Delta, v , Dxxyy,delta]
 prob = ODEProblem(rhsFun, u0, tspan, params)
 print("\n Solving....... \n")
-sol = solve(prob,saveat =.10, progress = true)
+sol = solve(prob,saveat =saveat, progress = true)
 print("\n Done! \n")
 
 
@@ -128,7 +130,7 @@ print("\n Save time series? (y/N)")
 ans = readline(stdin)
 if ans == "Y" || ans == "y"
     print("Saving data...")
-npzwrite("/home/james/PhD_Work/Python_Code/Brain_Top_Paper/Movie_Scripts/data/2D_planarwaves.npy", Z[:,:,:])
+npzwrite("/home/james/PhD_Work/Python_Code/Brain_Top_Paper/Movie_Scripts/data/2D_TuringHopf_kappaV=3.npy", Z[:,:,:])
 end
 
 
